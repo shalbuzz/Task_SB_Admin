f6c584a3c019073f9db6dc96fa94b328fce3c7a5
shalbuzz <rovshannashvor@gmail.com>
2025-05-02 02:28:08 +0400

    channges

--- Task_SB_Admin/obj/Release/net8.0/Task_SB_Admin.AssemblyInfo.cs
+++ Task_SB_Admin/obj/Release/net8.0/Task_SB_Admin.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task_SB_Admin")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7395638e7e596afaa64f3347c387fdc9fc86d66")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task_SB_Admin")]
